ec9b920ff877325817f2c72d5b9ec069a55a9c67
Isaac Park <isaac.a.park@gmail.com>
2016-07-01 18:12:48 -0400

    Issues Fixed

--- modules/demux/mpeg/id3v2.c
+++ modules/demux/mpeg/id3v2.c
@@ -20,21 +20,28 @@
 #include "id3v2.h"
 
 input_title_t* get_title(demux_t* p_demux) {
-   input_title_t *title = vlc_input_title_New();
-   uint64_t start = stream_Tell(p_demux->s);
+  input_title_t *title = vlc_input_title_New();
+  uint64_t start = stream_Tell(p_demux->s);
 
-   stream_Seek(p_demux->s, 0);
+  if (stream_Seek(p_demux->s, 0) < 0) {
+    printf("Coudln't seek to the start of the file\n");
+    return NULL;
+  }
 
   chapters_t chaps = get_chapters(p_demux->s);
 
-  for(int i = 0; i < chaps.size; ++i) {
+  for (int i = 0; i < chaps.size; ++i) {
     seekpoint_t *p_seekpoint = vlc_seekpoint_New();
     p_seekpoint->i_time_offset = chaps.chapters[i].start_time * 1000;
     p_seekpoint->psz_name = chaps.chapters[i].title; // releasing responsibality for closing
     TAB_APPEND( title->i_seekpoint, title->seekpoint, p_seekpoint );
   }
-  stream_Seek(p_demux->s, start);
   p_demux->info.i_update |= INPUT_UPDATE_TITLE_LIST;
+
+  if (stream_Seek(p_demux->s, start) < 0) {
+    printf("Couldn't seek back to the starting location\n");
+  }
+
   return title;
 }
 
@@ -79,27 +86,46 @@ id3v2_header_t parse_header(stream_t *stream) {
   char identifier[4];
   identifier[3] = 0;
 
-  stream_Read(stream, identifier, 3);
+  if (stream_Read(stream, identifier, 3) < 3) {
+    printf("couldn't read anything from the stream\n");
+    header.valid = false;
+    return header;
+  }
 
   if (memcmp(identifier,"ID3", 3) != 0) {
-    header.valid = 0; //false
+    header.valid = false;
     printf("invalid header\n");
     return header;
   }
   unsigned char version[2];
-  stream_Read(stream, version, 2);
+
+  if (stream_Read(stream, version, 2) < 2) {
+    printf("coudln't read version info");
+    header.valid = false;
+    return header;
+  }
+
   header.version.major = version[0];
   header.version.minor = version[1];
 
 
   unsigned char flags;
-  stream_Read(stream, &flags, 1);
+  if (stream_Read(stream, &flags, 1) < 1) {
+    printf("coudln't read flags");
+    header.valid = false;
+    return header;
+  }
 
 
   unsigned char size[4];
-  stream_Read(stream, size, 4);
+  if (stream_Read(stream, size, 4) < 4) {
+    printf("coudln't read size");
+    header.valid = false;
+    return header;
+  }
 
   header.size = size[0]*128*128*128 + size[1]*128*128 + size[2]*128 + size[3];
+  header.valid = true;
   return header;
 }
 
@@ -111,11 +137,16 @@ id3v2_frame_header_t parse_frame_header(stream_t* stream) {
   char identifier[5];
   identifier[4] = 0;
 
-  stream_Read(stream, identifier, 4);
+  if (stream_Read(stream, identifier, 4) < 4) {
+    printf("failed to read frame id\n");
+    header.size = 0;
+    header.id[0] = 0;
+    return header;
+  }
   strcpy(header.id, identifier);
 
   unsigned char size[4];
-  if(stream_Read(stream, size, 4) != 4) {
+  if (stream_Read(stream, size, 4) != 4) {
     printf("something went wrong\n");
   }
   bool standard_size = true;
@@ -151,24 +182,36 @@ int skip_frame(stream_t* stream, id3v2_frame_header_t frame) {
 id3v2_chapter_frame_t parse_chapter(stream_t* stream) {
   id3v2_chapter_frame_t frame;
   for(int i=0; i<20; ++i) {
-    stream_Read(stream, (frame.id + i), 1);
+    if (stream_Read(stream, (frame.id + i), 1) < 1) {
+      printf("coudln't read char\n");
+      frame.id[i] = 0;
+      break;
+    }
 
     if (frame.id[i] == 0) {
       break;
     }
   }
   unsigned char time[4];
-  stream_Read(stream, time, 4);
+  if (stream_Read(stream, time, 4) < 4) {
+    printf("couldn't read time\n");
+  }
   frame.start_time =  time[3] + time[2]*128*2 + time[1]*128*128*4 + time[0]*128*128*8;
 
-  stream_Read(stream, time, 4);
+  if (stream_Read(stream, time, 4) < 4) {
+    printf("couldn't read time\n");
+  }
   frame.end_time =  time[3] + time[2]*128*2 + time[1]*128*128*4 + time[0]*128*128*8;
 
 
-  stream_Read(stream, time, 4);
+  if (stream_Read(stream, time, 4) < 4) {
+    printf("couldn't read time\n");
+  }
   frame.start_offset =  time[3] + time[2]*128*2 + time[1]*128*128*4 + time[0]*128*128*8;
 
-  stream_Read(stream, time, 4);
+  if (stream_Read(stream, time, 4) < 4) {
+    printf("couldn't read time\n");
+  }
   frame.end_offset =  time[3] + time[2]*128*2 + time[1]*128*128*4 + time[0]*128*128*8;
   return frame;
 }
@@ -179,10 +222,15 @@ chapter_t get_chapter(stream_t* stream, id3v2_frame_header_t frame) {
   chap.start_time = chap_frame.start_time;
   if (stream_Tell(stream) <= frame.start + frame.size + 10) {
     id3v2_frame_header_t frame = parse_frame_header(stream);
-    char type;
-    stream_Read(stream, &type, 1);
+
+    char type = -1;
+
+    if (stream_Read(stream, &type, 1) < 1) {
+      printf("couldn't read type");
+    }
+
     char *title = malloc(frame.size);
-    if(stream_Read(stream, title, frame.size) < frame.size) {
+    if (stream_Read(stream, title, frame.size) < frame.size) {
       printf("something went wrong" );
     };
     if (type == 1) {
@@ -193,7 +241,9 @@ chapter_t get_chapter(stream_t* stream, id3v2_frame_header_t frame) {
       chap.title = malloc(dstlen);
       const char * pIn = (char*) title;
       char * pOut = (char*) chap.title;
-      vlc_iconv(cd, &pIn, &srclen, &pOut, &dstlen);
+      if (vlc_iconv(cd, &pIn, &srclen, &pOut, &dstlen) == 0) {
+        printf("coudln't convert\n");
+      }
       chap.title[frame.size-dstlen] = 0;
       vlc_iconv_close(cd);
       free(title);
@@ -205,7 +255,8 @@ chapter_t get_chapter(stream_t* stream, id3v2_frame_header_t frame) {
       chap.title[0] = 0;
     }
   } else {
-    chap.title = 0;
+    chap.title = malloc(1);
+    chap.title[0] = 0;
   }
   return chap;
 }
